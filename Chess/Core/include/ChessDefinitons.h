e9fd7b900e68fe2ab5dae360ad366363de6f2727
tom <iza012209@gmail.com>
2025-08-16 02:00:46 +0700

    Fix declaration bug

--- Chess/Core/include/ChessDefinitons.h
+++ Chess/Core/include/ChessDefinitons.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <stdint.h>
 
+//Type definitions
+using u64 = unsigned long long;
+using ui = unsigned int;
+
 struct Move {
 	ui from;
 	ui to;
@@ -34,9 +38,8 @@ enum enumSquare {
 	a7, b7, c7, d7, e7, f7, g7, h7,
 	a8, b8, c8, d8, e8, f8, g8, h8
 };
-//Bitboard
-using u64 = unsigned long long;
-using ui = unsigned int;
+
+//Bitboard constants
 
 #define C64(constantU64) constantU64##ULL
 
